40de39858ff8ed8fd6e2a957caa0890908065c1d
Kyler Rosquist <krosquist@credible.com>
2022-05-31 11:12:06 -0600

    Removing accidental change to the angular demo.

--- angular/src/utilities/groupModifiers.ts
+++ angular/src/utilities/groupModifiers.ts
@@ -1,22 +1,22 @@
-import { OptionType, SelectField } from '@youngalfred/bowtie-sdk';
-import { AppField, AppFieldGroup, GroupOrField } from 'src/types';
+import { OptionType, SelectField } from "@youngalfred/bowtie-sdk";
+import { AppField, AppFieldGroup, GroupOrField } from "src/types";
 
 // The tests expect "-" separating words instead of "."s
-export const makeTestId = (id: string) => id.replace(/\./g, '-');
+export const makeTestId = (id: string) => id.replace(/\./g, "-");
 
 const toRadioGroup = (fg: AppFieldGroup): AppFieldGroup => ({ // Transform the select question to a radio button group
     ...fg,
     // Should just be one child to reduce (ex: "house-type" and "construction-type")
     // but around 10 select options contained in that single child;
     children: fg.children.reduce((acc: GroupOrField[], field: GroupOrField): GroupOrField[] => {
-        const { options = [] } = field as Pick<SelectField, 'options'>;
+        const { options = [] } = field as Pick<SelectField, "options">;
         // Make radio buttons out of the select question's options
         return [...acc, ...options.map((option: OptionType, idx: number) => {
             return {
                 ...field,
                 id: `${field.id}.${option.name}`,
                 label: option.label,
-                kind: 'radio',
+                kind: "radio",
                 options: [option],
                 testId: makeTestId(`${field.id}.${idx + 1}`),
             };
@@ -26,7 +26,6 @@ const toRadioGroup = (fg: AppFieldGroup): AppFieldGroup => ({ // Transform the s
 
 
 export const uniqueFGs: Record<string, (fg: AppFieldGroup) => AppFieldGroup> = {
-    'house-type': toRadioGroup,
-    'construction-type': toRadioGroup,
-    'policy-type': toRadioGroup,
+    "house-type": toRadioGroup,
+    "construction-type": toRadioGroup,
 }
