68fd1b7dc6e5892bd59f4e5a27690306905486f9
Kyler Rosquist <krosquist@credible.com>
2022-07-14 15:02:04 -0600

    TLAN-1976: Removed unused files.

--- vue/src/decorators/information/subtitles.ts
+++
@@ -1,7 +0,0 @@
-
-// more subtitles to follow
-export const subtitles = {
-    'home.dateOfPurchase': 'If you have not yet purchased this property, put your expected closing date.',
-};
-
-export default subtitles;
